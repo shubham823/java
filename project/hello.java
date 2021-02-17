2614e7929e039de271b60f77157e8925e0d61daa
shubham <gupta.shubham823@gmail.com>
2021-02-17 11:47:12 +0530

    develop second commit

--- project/hello.java
+++ project/hello.java
@@ -3,7 +3,9 @@ public class hello{
 
 
 	public static void main (String Args[]){
-		System.out.print("hello");
+		System.out.print("hello master");
+
+
 		System.out.print("hello i am in develop");
 	}
 }
